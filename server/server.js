920c97eba3747a8b401941c28d2d9bfa294c6f38
Segey <firstthinker1989@gmail.com>
2017-09-17 13:37:43 +0500

    fix

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+app.set('port', process.env.PORT || 5000);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
@@ -23,6 +24,6 @@ app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(app.get('port'), () => {
   console.log('start');
 });
